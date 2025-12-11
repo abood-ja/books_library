0837d76b5b0136dc22b8a00b861c8162ac370e81
ZaidMra3be <Zaidmaraabeh@Icloud.com>
2025-12-11 15:56:18 +0300

    Add sonarcloud GitHub Actions workflow

--- src/main/java/aboodZaidLibrary/Main.java
+++ src/main/java/aboodZaidLibrary/Main.java
@@ -1,12 +1,11 @@
-//package aboodZaidLibrary;
-//
-//public class  Main {
-//	   public static void main(String[] args) {
-//
-//           while (true) {
-//               if (!clsLoginScreen.showLoginScreen()) break;
-//           }
-//
-//	    }
-//
-//}
+package aboodZaidLibrary;
+
+public class  Main {
+	   public static void main(String[] args) {
+
+           while (true) {
+               if (!clsLoginScreen.showLoginScreen()) break;
+           }
+}
+
+}
